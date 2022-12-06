9a7b04fc6ad525e24c41f072b78aa6a0f207e7d8
eaglelizard <whitedtd@gmail.com>
2022-12-06 11:24:45 -0700

    another pass at stripping the headers correctly

--- src/lib/txt2/parse-books/strip-gutenberg.ts
+++ src/lib/txt2/parse-books/strip-gutenberg.ts
@@ -7,19 +7,41 @@ import { ScrapedBookWithFile } from '../books/books-service';
 import { readFileStream } from './read-file-stream';
 
 export async function stripGutenbergBook(book: ScrapedBookWithFile) {
-  let startTag: boolean, endTag: boolean;
-  startTag = false;
-  endTag = false;
+  let startTagParsed: boolean, endTagParsed: boolean;
+  let parseStartTag: boolean, parseEndTag: boolean;
+  let startTagLineCount: number;
+
+  let startTagLine: string;
+
+  startTagLineCount = 0;
+
+  startTagParsed = false;
+  endTagParsed = false;
   const lineCb = (rawLine: string) => {
     let line: string;
     line = rawLine.trim().toLowerCase();
-    if(!startTag) {
-      if(checkGutenbergStart(rawLine)) {
-        startTag = true;
+    // line = rawLine;
+    if(!startTagParsed) {
+      if(startTagRx(line)) {
+        parseStartTag = true;
       }
-    } else if(!endTag) {
-      if(checkGutenbergEnd(rawLine)) {
-        endTag = true;
+    } else if(!endTagParsed) {
+      if(endTagRx(line)) {
+        parseEndTag = true;
+      }
+    }
+    if(parseStartTag) {
+      startTagLineCount++;
+      if(startTagEndRx(line)) {
+        // console.log(line);
+        parseStartTag = false;
+        startTagParsed = true;
+      }
+    }
+    if(parseEndTag) {
+      if(endTagEndRx(line)) {
+        parseEndTag = false;
+        endTagParsed = true;
       }
     }
   };
@@ -27,14 +49,41 @@ export async function stripGutenbergBook(book: ScrapedBookWithFile) {
   await readFileStream(book.filePath, {
     lineCb,
   });
-  if(!startTag || !endTag) {
-    console.log(`Missing start or end tags for ${book.fileName}`);
+  if(!startTagParsed || !endTagParsed) {
+    console.log('');
+    console.log(book.fileName);
+    if(!startTagParsed) {
+      console.log('Missing start tags');
+      // console.log(startTagLine);
+    }
+    if(!endTagParsed) {
+      console.log('Missing end tags');
+    }
   }
 }
 
-function checkGutenbergStart(line: string): boolean {
-  return (/^\*{3}(.)*start(.)*gutenberg(.)*\*{3}$/gi).test(line);
+function smallPrintRx(line: string): boolean {
+  return (/^\**.+(?:start|end).*small.*print/gi).test(line);
 }
-function checkGutenbergEnd(line: string): boolean {
-  return (/^\*{3}(.)*end(.)*gutenberg(.)*\*{3}$/gi).test(line);
+
+function endTagRx(line: string): boolean {
+  return (/^\*{3}\s*end(.)*gutenberg/gi).test(line);
 }
+function endTagEndRx(line: string): boolean {
+  return (/[^*]\*{3}$/gi).test(line);
+}
+
+function startTagRx(line: string): boolean {
+  // return (/^\*{3}\s*start/gi).test(line);
+  return (/^\*{3}\s*start(.)*gutenberg/gi).test(line);
+}
+function startTagEndRx(line: string): boolean {
+  return (/[^*]\*{3}$/gi).test(line);
+}
+
+// function checkGutenbergStart(line: string): boolean {
+//   return (/^\*{3}(.)*start(.)*gutenberg(.)*\*{3}$/gi).test(line);
+// }
+// function checkGutenbergEnd(line: string): boolean {
+//   return (/^\*{3}(.)*end(.)*gutenberg(.)*\*{3}$/gi).test(line);
+// }
