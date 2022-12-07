93556309a92cc0a9397665cbce30565871962fb6
eaglelizard <whitedtd@gmail.com>
2022-12-07 12:07:39 -0700

    Improve gutenberg header/footer script by scanning chars after regex detection

--- src/lib/txt2/parse-books/strip-gutenberg.ts
+++ src/lib/txt2/parse-books/strip-gutenberg.ts
@@ -6,6 +6,8 @@
 import { ScrapedBookWithFile } from '../books/books-service';
 import { readFileStream } from './read-file-stream';
 
+const GUTENBERG_TAG_MARKER = '*';
+
 export async function stripGutenbergBook(
   book: ScrapedBookWithFile,
   opts: {
@@ -22,47 +24,87 @@ export async function stripGutenbergBook(
   let hasSmallPrint: boolean;
   let hasErr: boolean;
 
+  let parseStartTagEndMarkers: boolean, startTagEndMarkerCount: number;
+  let parseEndTagEndMarkers: boolean, endTagEndMarkerCount: number;
+
+  parseStartTagEndMarkers = false;
+  startTagEndMarkerCount = 0;
+  parseEndTagEndMarkers = false;
+  endTagEndMarkerCount = 0;
+
   startTagParsed = false;
   endTagParsed = false;
+
   const lineCb = (rawLine: string) => {
     let line: string;
+    let startTagRxExecArr: RegExpExecArray, startTagCursor: number;
+    let endTagRxExecArr: RegExpExecArray, endTagCursor: number;
     // line = rawLine.trim().toLowerCase();
+    startTagCursor = 0;
+    endTagCursor = 0;
+
     line = rawLine;
     if(hasSmallPrint) {
       return;
     }
     if(!startTagParsed) {
-      if(startTagRx(line)) {
+      // if(line.includes('GUTENBERG')) {
+      //   console.log(line);
+      // }
+      if(getStartTagRx().test(line)) {
+        startTagRxExecArr = getStartTagRx().exec(line);
+        startTagCursor = startTagRxExecArr.index + startTagRxExecArr[0].length;
         parseStartTag = true;
       } else if(smallPrintRx(line)) {
         hasSmallPrint = true;
       }
     } else if(!endTagParsed) {
-      if(endTagRx(line)) {
+      if(getEndTagRx().test(line)) {
+        endTagRxExecArr = getEndTagRx().exec(line);
+        endTagCursor = endTagRxExecArr.index + endTagRxExecArr[0].length;
         parseEndTag = true;
       }
     }
     if(parseStartTag) {
-      if(startTagEndRx(line)) {
-        // console.log(line);
-        parseStartTag = false;
-        startTagParsed = true;
+      // console.log(line.substring(startTagCursor));
+      for(let i = startTagCursor; i < line.length; ++i) {
+        if(line[i] === GUTENBERG_TAG_MARKER) {
+          parseStartTagEndMarkers = true;
+          startTagEndMarkerCount++;
+        } else if(parseStartTagEndMarkers) {
+          parseStartTagEndMarkers = false;
+          startTagEndMarkerCount = 0;
+        }
+        if(startTagEndMarkerCount === 3) {
+          parseStartTag = false;
+          startTagParsed = true;
+        }
       }
-    }
-    if(parseEndTag) {
-      if(endTagEndRx(line)) {
-        parseEndTag = false;
-        endTagParsed = true;
+    } else if(parseEndTag) {
+      for(let i = endTagCursor; i < line.length; ++i) {
+        if(line[i] === GUTENBERG_TAG_MARKER) {
+          parseEndTagEndMarkers = true;
+          endTagEndMarkerCount++;
+        } else if(parseEndTagEndMarkers) {
+          parseEndTagEndMarkers = false;
+          endTagEndMarkerCount = 0;
+        }
+        if(endTagEndMarkerCount === 3) {
+          parseEndTag = false;
+          endTagParsed = true;
+        }
       }
     }
   };
 
   await readFileStream(book.filePath, {
     lineCb,
+    // lineCb: _lineCb,
   });
   hasErr = !startTagParsed || !endTagParsed || hasSmallPrint;
 
   if(hasErr) {
+    // console.log(`${book.fileName}`);
     if(hasSmallPrint) {
       // console.log('__Small print');
     }
@@ -85,6 +127,13 @@ export async function stripGutenbergBook(
   }
 }
 
+function getEndTagRx(): RegExp {
+  return (/^\s*\*{3}\s*end(.)*gutenberg/gi);
+}
+function getStartTagRx(): RegExp {
+  return /^\s*\*{3}\s*start(.)*gutenberg/gi;
+}
+
 function smallPrintRx(line: string): boolean {
   return (/^\**.+(?:start|end).*small.*print/gi).test(line);
 }
@@ -97,16 +146,8 @@ function endTagEndRx(line: string): boolean {
 }
 
 function startTagRx(line: string): boolean {
-  // return (/^\*{3}\s*start/gi).test(line);
   return (/^\*{3}\s*start(.)*gutenberg/gi).test(line);
 }
 function startTagEndRx(line: string): boolean {
   return (/[^*]\*{3}$/gi).test(line);
 }
-
-// function checkGutenbergStart(line: string): boolean {
-//   return (/^\*{3}(.)*start(.)*gutenberg(.)*\*{3}$/gi).test(line);
-// }
-// function checkGutenbergEnd(line: string): boolean {
-//   return (/^\*{3}(.)*end(.)*gutenberg(.)*\*{3}$/gi).test(line);
-// }
