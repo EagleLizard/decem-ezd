6b02430afecf751a456281afc1fef8ffc598e330
eaglelizard <whitedtd@gmail.com>
2022-12-16 09:58:37 -0700

    Add character parser

--- src/lib/txt2/parse-books/parse-books.ts
+++ src/lib/txt2/parse-books/parse-books.ts
@@ -3,6 +3,8 @@ import { Dirent } from 'fs';
 import { readdir } from 'fs/promises';
 import path from 'path';
 
+import _chunk from 'lodash.chunk';
+
 import { EBOOKS_DATA_DIR_PATH, STRIPPED_EBOOKS_DIR_PATH } from '../../../constants';
 import { getIntuitiveTimeString } from '../../../util/print-util';
 import { Timer } from '../../../util/timer';
@@ -26,8 +28,15 @@ export async function parseBooksMain() {
 }
 
 async function countParseBooksHandler() {
+  let baseDir: string;
   let txtBooksMeta: ScrapedBookWithFile[], scrapedBooks: ScrapedBookWithFile[];
-  let bookToParse: ScrapedBookWithFile[];
+  let booksToParse: ScrapedBookWithFile[];
+
+  // baseDir = EBOOKS_DATA_DIR_PATH;
+  baseDir = STRIPPED_EBOOKS_DIR_PATH;
+
+  const _getBookPath = getBookPathMemo(baseDir);
+
   txtBooksMeta = await getTxtBookMeta();
   scrapedBooks = txtBooksMeta.filter(bookMeta => {
     return (
@@ -40,28 +49,12 @@ async function countParseBooksHandler() {
     );
   });
 
-  await parseBooksSync(scrapedBooks);
-}
+  booksToParse = [];
 
-async function parseBooksSync(booksToParse: ScrapedBookWithFile[]) {
-  let books: ScrapedBookWithFile[], baseDir: string;
-  let doneCount: number, totalLineCount: number;
-  let donePrintMod: number;
-  let parseTimer: Timer, parseMs: number;
-
-  // baseDir = EBOOKS_DATA_DIR_PATH;
-  baseDir = STRIPPED_EBOOKS_DIR_PATH;
-
-  const _getBookPath = getBookPathMemo(baseDir);
-
-  totalLineCount = 0;
-  doneCount = 0;
-  books = [];
-
-  for(let i = 0; i < booksToParse.length; ++i) {
+  for(let i = 0; i < scrapedBooks.length; ++i) {
     let currBook: ScrapedBookWithFile, foundBookPath: string;
     let nextBook: ScrapedBookWithFile;
-    currBook = booksToParse[i];
+    currBook = scrapedBooks[i];
     foundBookPath = await _getBookPath(currBook);
     if(foundBookPath === undefined) {
       continue;
@@ -70,8 +63,172 @@ async function parseBooksSync(booksToParse: ScrapedBookWithFile[]) {
       ...currBook,
       filePath: foundBookPath,
     };
-    books.push(nextBook);
+    booksToParse.push(nextBook);
   }
+
+  // await parseLineCountsSync(booksToParse, baseDir);
+  await parseCharCountsSync(booksToParse, baseDir);
+  // await parseCharCountsChunked(booksToParse, baseDir);
+}
+
+async function parseCharCountsChunked(books: ScrapedBookWithFile[], baseDir: string) {
+  let doneCount: number, totalCharCount: number, totalLineCount: number;
+  let bookChunks: ScrapedBookWithFile[][];
+  let donePrintMod: number;
+  let parseTimer: Timer, parseMs: number;
+
+  const CHAR_COUNT_BOOK_CHUNK_SIZE = 10;
+  console.log(`CHAR_COUNT_BOOK_CHUNK_SIZE: ${CHAR_COUNT_BOOK_CHUNK_SIZE}`);
+
+  totalCharCount = 0;
+  totalLineCount = 0;
+  doneCount = 0;
+
+  donePrintMod = Math.ceil(books.length / 70);
+
+  const doneCb = (opts: CharCountParseCbResult) => {
+    doneCount++;
+    totalCharCount += opts.charCount;
+    totalLineCount += opts.lineCount;
+    if((doneCount % donePrintMod) === 0) {
+      process.stdout.write('+');
+    }
+  };
+
+  parseTimer = Timer.start();
+
+  bookChunks = _chunk(books, CHAR_COUNT_BOOK_CHUNK_SIZE);
+
+  for(let i = 0; i < bookChunks.length; ++i) {
+    let currBookChunk: ScrapedBookWithFile[], currBookChunkPromises: Promise<void>[];
+    currBookChunk = bookChunks[i];
+    currBookChunkPromises = [];
+    for(let k = 0; k < currBookChunk.length; ++k) {
+      let bookPromise: Promise<void>;
+      bookPromise = (async () => {
+        let currBook: ScrapedBookWithFile;
+        currBook = currBookChunk[k];
+        await charCountParse({
+          book: currBook,
+          bookDir: baseDir,
+          doneCb,
+        });
+      })();
+      currBookChunkPromises.push(bookPromise);
+    }
+    await Promise.all(currBookChunkPromises);
+  }
+
+  parseMs = parseTimer.stop();
+
+  console.log('');
+  console.log(`parsed ${doneCount.toLocaleString()} books in ${getIntuitiveTimeString(parseMs)}`);
+  console.log(`totalLines: ${totalLineCount.toLocaleString()}`);
+  console.log(`totalChars: ${totalCharCount.toLocaleString()}`);
+}
+
+async function parseCharCountsSync(books: ScrapedBookWithFile[], baseDir: string) {
+  let doneCount: number, totalCharCount: number, totalLineCount: number;
+  let donePrintMod: number;
+  let parseTimer: Timer, parseMs: number;
+
+  totalCharCount = 0;
+  totalLineCount = 0;
+  doneCount = 0;
+
+  donePrintMod = Math.ceil(books.length / 70);
+
+  const doneCb = (opts: CharCountParseCbResult) => {
+    doneCount++;
+    totalCharCount += opts.charCount;
+    totalLineCount += opts.lineCount;
+    if((doneCount % donePrintMod) === 0) {
+      process.stdout.write('+');
+    }
+  };
+
+  parseTimer = Timer.start();
+
+  for(let i = 0; i < books.length; ++i) {
+    let currBook: ScrapedBookWithFile;
+    currBook = books[i];
+    await charCountParse({
+      book: currBook,
+      bookDir: baseDir,
+      doneCb,
+    });
+  }
+
+  parseMs = parseTimer.stop();
+
+  console.log('');
+  console.log(`parsed ${doneCount.toLocaleString()} books in ${getIntuitiveTimeString(parseMs)}`);
+  console.log(`totalLines: ${totalLineCount.toLocaleString()}`);
+  console.log(`totalChars: ${totalCharCount.toLocaleString()}`);
+}
+
+type CharCountParseCbResult = {
+  book: ScrapedBookWithFile;
+  bookFilePath: string;
+  charCount: number;
+  lineCount: number;
+};
+
+type CharCountParseOpts = {
+  book: ScrapedBookWithFile;
+  bookDir: string;
+  doneCb: (result: CharCountParseCbResult) => void;
+};
+
+async function charCountParse(opts: CharCountParseOpts) {
+  let fileName: string, filePath: string;
+  let charCount: number, lineCount: number;
+
+  fileName = `${opts.book.fileName}.txt`;
+  filePath = [
+    opts.bookDir,
+    fileName
+  ].join(path.sep);
+
+  charCount = 0;
+  lineCount = 0;
+
+  const countLineChars = (line: string) => {
+    for(let i = 0; i < line.length; ++i) {
+      let currChar: string;
+      currChar = line[i];
+      if((/[\S]/gi).test(currChar)) {
+        charCount++;
+      }
+    }
+  };
+
+  const lineCb = (line: string) => {
+    lineCount++;
+    countLineChars(line);
+  };
+
+  await readFileStream(filePath, {
+    lineCb,
+  });
+
+  opts.doneCb({
+    book: opts.book,
+    bookFilePath: filePath,
+    charCount,
+    lineCount,
+  });
+}
+
+async function parseLineCountsSync(books: ScrapedBookWithFile[], baseDir: string) {
+  let doneCount: number, totalLineCount: number;
+  let donePrintMod: number;
+  let parseTimer: Timer, parseMs: number;
+
+  console.log('parseLineCountsSync');
+
+  totalLineCount = 0;
+  doneCount = 0;
 
   donePrintMod = Math.ceil(books.length / 70);
 
